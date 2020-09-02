753dadac6fe926a387e3febfcaa7f279a03818e1
Gleb Pomykalov <gleb@exogress.com>
2020-09-02 23:11:28 +0300

    support max_tunnels_count

--- crates/signaling/src/lib.rs
+++ crates/signaling/src/lib.rs
@@ -6,6 +6,7 @@ use smartstring::alias::String;
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct TunnelRequest {
     pub hostname: String,
+    pub max_tunnels_count: u16,
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
