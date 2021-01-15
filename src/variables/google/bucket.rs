febccc1b268f1b8f2a8a4e87031de41233e1e8a2
Gleb Pomykalov <gleb@exogress.com>
2021-01-15 12:05:21 +0300

    further work on variables

--- src/variables/google/bucket.rs
+++ src/variables/google/bucket.rs
@@ -2,7 +2,6 @@ use serde::{Deserialize, Serialize};
 use smol_str::SmolStr;
 
 #[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq, Hash)]
-#[serde(deny_unknown_fields)]
-pub struct GcsBucket {
-    pub bucket: SmolStr,
-}
+#[serde(transparent)]
+pub struct GcsBucket(SmolStr);
+
--- src/variables/google/credentials.rs
+++ src/variables/google/credentials.rs
@@ -2,8 +2,5 @@ use serde::{Deserialize, Serialize};
 use smol_str::SmolStr;
 
 #[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq, Hash)]
-#[serde(deny_unknown_fields)]
-pub struct GoogleCredentials {
-    // pub json: HashMap<SmolStr, SmolStr>,
-    pub json: SmolStr,
-}
+#[serde(deny_unknown_fields, transparent)]
+pub struct GoogleCredentials(SmolStr);
