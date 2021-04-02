ae8df7c85509ba176d44c994030769c3f82db982
Gleb Pomykalov <gleb@exogress.com>
2021-04-02 16:10:34 +0300

    add exceptions

--- src/entities/exceptions.rs
+++ src/entities/exceptions.rs
@@ -19,7 +19,8 @@ exceptions! {
     CONFIG_REFERENCE_NAME_NOT_DEFINED => "config-error", "reference-name-not-defined"
     CONFIG_SCHEMA_MISMATCH => "config-error", "schema-mismatch"
 
-    PROXY_BAD_GATEWAY => "proxy-error", "bad-gateway", "no-healthy-upstreams"
+    PROXY_BAD_GATEWAY => "proxy-error", "bad-gateway"
+    PROXY_BAD_GATEWAY_NO_HEALTHY_UPSTREAMS => "proxy-error", "bad-gateway", "no-healthy-upstreams"
     PROXY_UPSTREAM_UNREACHABLE => "proxy-error", "upstream-unreachable"
     PROXY_UPSTREAM_UNREACHABLE_CONNECTION_REJECTED => "proxy-error", "upstream-unreachable", "connection-rejected"
     PROXY_INSTANCE_UNREACHABLE => "proxy-error", "instance-unreachable"
@@ -35,4 +36,12 @@ exceptions! {
     STATIC_RESPONSE_REDIRECT_ERROR => "static-response-error", "redirect-error"
 
     MODIFICATION_ERROR => "modification-error"
+
+    S3_BAD_CONFIGURATION => "s3-error", "bad-configuration"
+
+    GCS_BAD_CONFIGURATION => "gcs-error", "config-error"
+    GCS_BAD_BUCKET_NAME => "gcs-error", "config-error", "bad-bucket-name"
+    GCS_BAD_OBJECT_NAME => "gcs-error", "config-error", "bad-object-name"
+    GCS_AUTH_ERROR_BUILD_REQUEST_EROR => "gcs-error", "auth-error", "build-request-error"
+    GCS_AUTH_ERROR_BAD_RESPONSE => "gcs-error", "auth-error", "bad-response"
 }
