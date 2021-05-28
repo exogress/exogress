b8931aa94be5f6fc6cb8e9b8e7c82fe4682f10c9
Gleb Pomykalov <gleb@exogress.com>
2021-05-28 18:56:57 +0300

    rework invalidation format to array

--- src/config_core/cache.rs
+++ src/config_core/cache.rs
@@ -1,14 +1,19 @@
 use crate::{config_core::Filter, entities::InvalidationGroupName};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-use std::collections::BTreeMap;
 
 #[derive(Serialize, Deserialize, Debug, Clone, Hash, PartialEq, JsonSchema)]
 pub struct Cache {
     pub enabled: bool,
 
     #[serde(default)]
-    pub invalidations: BTreeMap<InvalidationGroupName, Vec<Filter>>,
+    pub invalidations: Vec<Invalidation>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Hash, PartialEq, JsonSchema)]
+pub struct Invalidation {
+    pub name: InvalidationGroupName,
+    pub filters: Vec<Filter>,
 }
 
 impl Default for Cache {
