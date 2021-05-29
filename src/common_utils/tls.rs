c67e2af017286cdbfa5c7d73fe976f90b05a4fdf
Gleb Pomykalov <gleb@exogress.com>
2021-05-29 09:48:34 +0300

    Load native certs only once

--- src/common_utils/tls.rs
+++ src/common_utils/tls.rs
@@ -1,9 +1,18 @@
-pub fn load_native_certs_safe(tls_config: &mut rustls::ClientConfig) {
-    let roots = match rustls_native_certs::load_native_certs() {
-        Ok(store) | Err((Some(store), _)) => store.roots,
-        Err((None, _)) => Vec::new(),
+use lazy_static::lazy_static;
+use rustls::OwnedTrustAnchor;
+
+lazy_static! {
+    pub static ref NATIVE_CERTS: Vec<OwnedTrustAnchor> = {
+        let roots = match rustls_native_certs::load_native_certs() {
+            Ok(store) | Err((Some(store), _)) => store.roots,
+            Err((None, _)) => Vec::new(),
+        };
+        roots
     };
-    for root in roots {
-        tls_config.root_store.roots.push(root);
+}
+
+pub fn load_native_certs_safe(tls_config: &mut rustls::ClientConfig) {
+    for root in &*NATIVE_CERTS {
+        tls_config.root_store.roots.push(root.clone());
     }
 }
